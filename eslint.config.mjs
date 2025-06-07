070664e65cc675c3d4b4ed3d93055037df36d588
girafeev1 <girafeev1+github@gmail.com>
2025-06-08 04:13:51 +0800

    Add Jest setup and simplify ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
