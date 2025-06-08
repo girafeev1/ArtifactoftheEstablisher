75070bdced05b08570fa409bede9c7be6ba986ca
girafeev1 <girafeev1+github@gmail.com>
2025-06-09 00:48:29 +0800

    Add Firebase Auth integration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,6 +2,7 @@
 
 import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey:               process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
@@ -20,6 +21,7 @@ const app = !getApps().length
 
 // Connect to the "aote-ref" Firestore database within the aote-pms project
 export const db = getFirestore(app, 'aote-ref')
+export const auth = getAuth(app)
 console.log('[firebase] Firestore initialized', {
   projectId: db.app.options.projectId,
   database: 'aote-ref',
---
+++ lib/hooks/useFirebaseAuth.ts
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth'
+import { auth } from '../firebase'
+
+export function useFirebaseAuth() {
+  const { data: session } = useSession()
+
+  useEffect(() => {
+    const accessToken = session?.accessToken as string | undefined
+    if (!accessToken) return
+    if (auth.currentUser) return
+
+    const credential = GoogleAuthProvider.credential(null, accessToken)
+    signInWithCredential(auth, credential)
+      .then(userCred => {
+        console.log('[useFirebaseAuth] Signed in to Firebase as', userCred.user.email)
+      })
+      .catch(err => {
+        console.error('[useFirebaseAuth] Failed to sign in to Firebase', err)
+      })
+  }, [session])
+}
