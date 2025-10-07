b06691fbed3c264daa143d28d4472f21c49ac3d4
girafeev1 <girafeev1+github@gmail.com>
2025-10-08 01:02:25 +0800

    Refine client accounts list with alphabet filter

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,50 @@
-module.exports = {
+import path from "path";
+import type { NextConfig } from "next";
+
+const CLASSNAMES_FRAGMENT = "/antd/node_modules/classnames/index.js";
+
+const config: NextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  transpilePackages: [
+    "@refinedev/antd",
+    "@refinedev/core",
+    "@refinedev/nextjs-router",
+    "@ant-design/icons",
+    "antd",
+    "rc-util",
+    "rc-picker",
+    "rc-table",
+    "rc-tree",
+    "rc-pagination",
+    "rc-menu",
+    "rc-tabs",
+    "rc-select",
+    "rc-dropdown",
+  ],
+  webpack: (webpackConfig) => {
+    webpackConfig.module = webpackConfig.module ?? {};
+    webpackConfig.module.parser = webpackConfig.module.parser ?? {};
+    webpackConfig.module.parser.javascript = {
+      ...webpackConfig.module.parser.javascript,
+      importMeta: true,
+    };
+    webpackConfig.module.rules = webpackConfig.module.rules ?? [];
+    webpackConfig.module.rules.push({
+      test: (resource: string) =>
+        resource.replace(/\\/g, "/").endsWith(CLASSNAMES_FRAGMENT),
+      enforce: "post",
+      use: [
+        {
+          loader: path.resolve(__dirname, "scripts/replaceImportMetaLoader.js"),
+        },
+      ],
+    });
+    return webpackConfig;
+  },
 };
+
+export default config;
