10e196610099d0bc5e3630b7ad9bc72bb9af064e
Jeffero Chan <girafeev1@gmail.com>
2025-11-23 13:12:13 +0800

    feat: Add generated PDF styles and clean up build process

--- lib/pdfTemplates/generatedStyles.ts
+++ lib/pdfTemplates/generatedStyles.ts
@@ -1,41 +1,5 @@
-// This file is auto-generated. Do not edit.
+// This file is auto-generated from a Google Sheet. Do not edit.
 
 import { StyleSheet } from '@react-pdf/renderer';
 
-export const generatedStyles = StyleSheet.create({
-  "page": {
-    "fontFamily": "Roboto",
-    "fontSize": 11,
-    "padding": 40,
-    "flexDirection": "column"
-  },
-  "logoMark": {
-    "fontFamily": "Rampart One",
-    "fontSize": 60
-  },
-  "sectionLabel": {
-    "fontSize": 10
-  },
-  "billName": {
-    "fontSize": 12
-  },
-  "projectTitle": {
-    "fontSize": 14
-  },
-  "projectNature": {
-    "fontSize": 9
-  },
-  "tableHeader": {},
-  "tableColDesc": {},
-  "tableColAmount": {},
-  "itemRow": {},
-  "amountCell": {},
-  "totalsBlock": {},
-  "totalsRow": {},
-  "footer": {},
-  "footerZh": {},
-  "pageNumber": {},
-  "qrContainer": {},
-  "invoiceLabel": {},
-  "headerRow": {}
-});
+export const generatedStyles = StyleSheet.create({});
